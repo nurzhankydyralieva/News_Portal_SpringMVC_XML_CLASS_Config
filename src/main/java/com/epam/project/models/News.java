96921ac8121a0289cdad861a8a3157db890e1c73
Nurzhan <nurzhankydyralieva@gmail.com>
2023-03-24 12:49:43 +0600

    Added Internationalization (i18n) and Localization (L10n) and Hibernate

--- src/main/java/com/epam/project/models/News.java
+++ src/main/java/com/epam/project/models/News.java
@@ -2,25 +2,36 @@ package com.epam.project.models;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
-
+@Entity
+@Table(name = "News")
 public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
     @NotEmpty(message = "News Title should not be empty")
     @Size(min = 10, max = 50, message = "News Title should be between 10 to 200 characters")
+    @Column(name = "newsTitle")
     private String newsTitle;
+    @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "dd/MM/yyyy")
+    @Column(name = "newsDate")
     private Date newsDate;
-
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "createdAt")
     private Date createdAt;
     @NotEmpty(message = "Brief should not be empty")
     @Size(min = 10, max = 500, message = "Brief should be between 10 to 500 characters")
+    @Column(name = "brief")
     private String brief;
     @NotEmpty(message = "Content should not be empty")
     @Size(min = 10, max = 1000, message = "Content should be between 20 to 1000 characters")
+    @Column(name = "content")
     private String content;
 
     public News() {
