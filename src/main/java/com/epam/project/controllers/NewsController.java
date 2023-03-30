d1aca7d5bbde69baa90cda1c94936666c86d110a
Nurzhan <nurzhankydyralieva@gmail.com>
2023-03-30 17:29:04 +0600

    Added Spring Data Jpa with XML configuration

---
+++ src/main/java/com/epam/project/controllers/NewsController.java
@@ -0,0 +1,73 @@
+package com.epam.project.controllers;
+
+import com.epam.project.models.News;
+import com.epam.project.services.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/")
+public class NewsController {
+    private final NewsService newsService;
+
+    @Autowired
+    public NewsController(NewsService newsService) {
+        this.newsService = newsService;
+    }
+
+    @GetMapping
+    public String indexPage(Model model) {
+        model.addAttribute("news", newsService.findAll());
+        return "index";
+    }
+
+    @GetMapping("/news/{id}")
+    public String showNews(@PathVariable("id") int id, Model model) {
+        model.addAttribute("newsPage", newsService.findNewsById(id));
+        return "viewNews";
+    }
+
+    @GetMapping("/news/new")
+    public String newNews(@ModelAttribute("newsPage") News news) {
+        return "newNews";
+    }
+
+    @PostMapping("/news")
+    public String createNews(@ModelAttribute("newsPage") @Valid News news,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "newNews";
+        }
+
+        newsService.save(news);
+        return "redirect:/";
+    }
+
+    @GetMapping("/news/{id}/edit")
+    public String editNews(Model model, @PathVariable("id") int id) {
+        model.addAttribute("newsPage", newsService.findNewsById(id));
+        return "editNews";
+    }
+
+    @PatchMapping("/news/{id}")
+    public String updateNews(@ModelAttribute("newsPage") @Valid News news,
+                             BindingResult bindingResult, @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "editNews";
+        }
+        newsService.updateNews(id, news);
+        return "redirect:/";
+    }
+
+
+    @DeleteMapping("/news/{id}")
+    public String deleteNews(@PathVariable("id") int id) {
+        newsService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/epam/project/services/NewsService.java
@@ -0,0 +1,45 @@
+package com.epam.project.services;
+
+import com.epam.project.models.News;
+import com.epam.project.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class NewsService {
+
+    private final NewsRepository newsRepository;
+
+    @Autowired
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public List<News> findAll() {
+        return newsRepository.findAll();
+    }
+
+    public News findNewsById(int id){
+        Optional<News> foundNews = newsRepository.findById(id);
+        return foundNews.orElse(null);
+    }
+
+    @Transactional
+    public void save(News news){
+        newsRepository.save(news);
+    }
+    @Transactional
+    public void updateNews(int id,News updatedNews){
+        updatedNews.setId(id);
+        newsRepository.save(updatedNews);
+    }
+    @Transactional
+    public void delete(int id){
+        newsRepository.deleteById(id);
+    }
+}
