993220be8ab18192c86e494d6d5346ce9de3bd80
Nurzhan <nurzhankydyralieva@gmail.com>
2023-10-16 17:57:16 +0600

    added jacoco

---
+++ src/test/java/com/epam/project/services/NewsServiceTest.java
@@ -0,0 +1,40 @@
+package com.epam.project.services;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class NewsServiceTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+
+    public void testFindAll() {
+    }
+
+    @Test
+
+    public void testFindById() {
+    }
+
+    @Test
+    public void testSave() {
+    }
+
+    @Test
+    public void testUpdateNews() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testDeleteNewsById() {
+    }
+}
